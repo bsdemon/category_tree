8bd791d181602c69bbd8c272221dd110c744d80e
Iliya Iliev <bsdemon@gmail.com>
2025-12-05 23:51:09 +0200

    Admin models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,24 @@
+# api/admin.py
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, CategorySimilarity
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "parent", "path")
+    list_filter = ("parent",)
+    search_fields = ("name", "description", "path")
+    raw_id_fields = ("parent",)
+    ordering = ("path",)
+
+    # Optional: readonly path if не искаш да се пипа ръчно в админ
+    readonly_fields = ("path",)
+
+
+@admin.register(CategorySimilarity)
+class CategorySimilarityAdmin(admin.ModelAdmin):
+    list_display = ("id", "category1", "category2")
+    search_fields = ("category1__name", "category2__name")
+    raw_id_fields = ("category1", "category2")
+    ordering = ("category1", "category2")
